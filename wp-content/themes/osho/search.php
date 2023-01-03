0583db53d4250f57f7ffa29e1911e7220678b5b0
sajin manandhar <sajin@itoffer.dk>
2023-01-03 10:53:22 +0545

    search page updated

--- wp-content/themes/osho/search.php
+++ wp-content/themes/osho/search.php
@@ -10,35 +10,51 @@ global $wp_query;
 //print_r($wp_query);
 //wp_die();
 ?>
-	<div id="primary">
-		<main id="main" class="site-main mt-5" role="main">
-			<div class="container">
-				<header class="mb-5">
-					<h1 class="page-title"> <?php echo $wp_query->found_posts; ?>
-						<?php _e( 'Search Results Found For', 'locale' ); ?>: "<?php the_search_query(); ?>"
-					</h1>
-				</header>
+<div class="page-banner" style="background: url(<?php the_post_thumbnail_url(); ?>">
+    <div class="container">
+        <section class="pb-sec pb-2">
+            <h2><?php the_title(); ?></h2>
+            <nav aria-label="breadcrumb" class="page-menu">
+                <ul class="breadcrumb">
+                    <li><a href="<?php echo site_url('/'); ?>">Home</a></li>
+                    <li><a href="<?php echo site_url('/audio'); ?>">Audio Books</a></li>
+                    <li><?php the_title(); ?></li>
+                </ul>
+            </nav>
+        </section>
+    </div>
+</div>
+<div class="search-result">
+    <main class="result-content">
+        <div class="container">
+            <section class="search-sec">
 
-				<?php if ( have_posts() ) { ?>
 
-					<div>
+                <h3 class="page-title"> <?php echo $wp_query->found_posts; ?>
+                    <?php _e( 'Search Results Found For', 'locale' ); ?>: "<?php the_search_query(); ?>"
+                </h3>
 
-						<?php while ( have_posts() ) {
+                <?php if ( have_posts() ) { ?>
+                    <div class="ab-lists">
+
+                <div class="row">
+
+                    <?php while ( have_posts() ) {
 							the_post(); ?>
-							<div class="column">
-                <div class="slide-item ab-card">
-                    <a href="<?php the_permalink();?>">
-                        <div class="ab-img">
-                            <img src="<?php the_post_thumbnail_url();?>" class="img-fluid">
-                        </div>
-                        <div class="ab-brief">
-                            <ul class="meta-tag">
-                                <li><?php echo get_the_date('F')?> <?php echo get_the_date('d');?>
-                                    <?php echo get_the_date('Y');?></li>
-                            </ul>
-                            <h2><?php the_title();?></h2>
-                            <!-- <span class="total-chapter">1 Chapters</span>  -->
-                            <?php
+                    <div class="column">
+                        <div class="slide-item ab-card">
+                            <a href="<?php the_permalink();?>">
+                                <div class="ab-img">
+                                    <img src="<?php the_post_thumbnail_url();?>" class="img-fluid">
+                                </div>
+                                <div class="ab-brief">
+                                    <ul class="meta-tag">
+                                        <li><?php echo get_the_date('F')?> <?php echo get_the_date('d');?>
+                                            <?php echo get_the_date('Y');?></li>
+                                    </ul>
+                                    <h2><?php the_title();?></h2>
+                                    <!-- <span class="total-chapter">1 Chapters</span>  -->
+                                    <?php
 
                             $playlists = get_field('album_playlist', $post->ID);
                             $total_tracks = 0; 
@@ -56,22 +72,22 @@ global $wp_query;
                             echo '<span class="total-chapter">' . $total_tracks . ' Chapters</span>';
 
                             ?>
+                                </div>
+                            </a>
                         </div>
-                    </a>
+                    </div>
+
+                    <?php } ?>
+
                 </div>
-                </div>
+                    </div>
 
-						<?php } ?>
 
-					</div>
-
-					
-
-				<?php } else {
+                <?php } else {
 					get_search_form();
 				}?>
-
-			</div>
-		</main>
-	</div>
+            </section>
+        </div>
+    </main>
+</div>
 <?php get_footer(); ?>
