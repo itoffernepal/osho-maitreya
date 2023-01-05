881ce5762729dc4baa3efc2ad92395cd9aeb869e
sajin manandhar <sajin@itoffer.dk>
2023-01-05 11:25:50 +0545

    design updated

--- wp-content/themes/osho/front-page.php
+++ wp-content/themes/osho/front-page.php
@@ -75,16 +75,10 @@
                         global $post;
                 ?>
 
-<<<<<<< HEAD
                 <div class="slide-item ab-card">
                     <a href="<?php the_permalink(); ?>">
-                        <div class="ab-img">
-                            <img src="<?php the_post_thumbnail_url(); ?>" class="img-fluid">
-=======
-                        <div class="slide-item ab-card">
-                            <a href="<?php the_permalink(); ?>">
-                            <div class="ab-img">
-                            <?php 
+                    <div class="ab-img">
+                            <?php
                                 $thumbnail_url = get_the_post_thumbnail_url();
                                 if ( empty( $thumbnail_url ) ) {
                                 $thumbnail_url = get_template_directory_uri() . '/images/placeholder.png';
@@ -92,29 +86,6 @@
                             ?>
                             <img src="<?php echo esc_url( $thumbnail_url ); ?>" class="img-fluid">
                             </div>
-
-                                <div class="ab-brief">
-                                    <ul class="meta-tag">
-                                        <li><?php echo get_the_date('F') ?> <?php echo get_the_date('d'); ?>
-                                            <?php echo get_the_date('Y'); ?></li>
-                                    </ul>
-                                    <h2><?php the_title(); ?></h2>
-                                    <!-- <span class="total-chapter">1 Chapters</span>  -->
-                                    <!-- number of chapter in album code start -->
-                                    <?php $playlists = get_field('album_playlist', $post->ID); ?>
-                                    <?php if (isset($playlists) && !empty($playlists)) : ?>
-                                        <?php $tracks = get_post_meta($playlists[0], '_audioigniter_tracks', true); ?>
-                                        <?php if (isset($tracks) &&  !empty($tracks)) : ?>
-                                            <?php //$unserliazedTracks = unserialize($tracks);
-                                            ?>
-                                            <span class="total-chapter"><?php echo count($tracks); ?> Chapters</span>
-                                        <?php endif; ?>
-                                    <?php endif; ?>
-                                    <!-- number of chapter in album code end -->
-                                </div>
-                            </a>
->>>>>>> d4a91729b7de528f8e7792b0176e52c3c1b0220b
-                        </div>
                         <div class="ab-brief">
                             <ul class="meta-tag">
                                 <li><?php echo get_the_date('F') ?> <?php echo get_the_date('d'); ?>
